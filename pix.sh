eb8c5f0549c1a7459d880cd929ececa2de711030
Luis Olmedo <olmedoluis012@gmail.com>
2020-05-05 22:47:30 -0300

    refactor(pix.sh): Execute status by default

--- pix.sh
+++ pix.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "hello world"
+path="/home/luis/Documents/Projects/pix-bash"
+
+$path/src/API/Git/Status/Status.py
+
+# /home/luis/Documents/Projects/pix-bash/src/API/Git/Status/Status.py
+
+# if [ $1 = "w" ];
+# the
+# fi
