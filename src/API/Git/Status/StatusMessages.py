07e06aec49f3ec83a345ec23914e8413987c5d70
Luis Olmedo <olmedoluis012@gmail.com>
2020-05-06 21:43:51 -0300

    refactor(Status): Deleted deprecated files

--- src/API/Git/Status/StatusMessages.py
+++
@@ -1,7 +0,0 @@
-MESSAGES = {
-    "branch": " Branch: {}\n",
-    "untracked": " Untracked files:\n",
-    "renamed": " Renamed files:\n",
-    "added": " added files:\n",
-    "modified": " Modified files:\n"
-}
