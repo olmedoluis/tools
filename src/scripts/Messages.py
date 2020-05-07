c624f83093756030acfb4346ca4a56c6a46eb4d8
Luis Olmedo <olmedoluis012@gmail.com>
2020-05-07 00:32:27 -0300

    refactor(Messages.py): Show messages with colors

--- src/scripts/Messages.py
+++ src/scripts/Messages.py
@@ -1,10 +1,22 @@
+from Theme import THEME
+
+
+def themeUp(string):
+    return string.format(**THEME).replace("$", "{}") + THEME["reset"]
+
+
 MESSAGES = {
-    "branch": " Branch: {}",
-    "untracked": "\n Untracked files:\n",
-    "renamed": "\n Renamed files:\n",
-    "added": "\n Added files:\n",
-    "modified": "\n Modified files:\n",
-    "deleted": "\n Deleted files:\n",
-    "change": "\t{}",
-    "clean": "\n Congratulations!\n You are clean!\n"
+    "added-title": "\n {}\n".format(themeUp("{normal}Added files:")),
+    "branch": " {} {}".format(themeUp("{normal}Branch:"), themeUp("{keyword}$")),
+    "change": "\t{}".format(themeUp("{success}$")),
+    "clean": "\n {}\n {}".format(themeUp("{normal}Congratulations!"), themeUp("{success}You are clean!")),
+    "deleted-title": "\n {}\n".format(themeUp("{normal}Deleted files:")),
+    "modified-title": "\n {}\n".format(themeUp("{normal}Modified files:")),
+    "renamed-title": "\n {}\n".format(themeUp("{normal}Renamed files:")),
+    "untracked-title": "\n {}\n".format(themeUp("{normal}Untracked files:")),
+    "deleted": "\t{}".format(themeUp("{normal}$")),
+    "modified": "\t{}".format(themeUp("{modified}$")),
+    "renamed": "\t{}".format(themeUp("{renamed}$")),
+    "untracked": "\t{}".format(themeUp("{untracked}$")),
+    "added": "\t{}".format(themeUp("{added}$")),
 }
