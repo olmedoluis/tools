ad5461847c976dbba4d28b0c34ba37019617d6ef
Luis Olmedo <olmedoluis012@gmail.com>
2020-05-07 22:00:38 -0300

    feat(Messages.py): Add icons to messages

--- src/scripts/Messages.py
+++ src/scripts/Messages.py
@@ -7,16 +7,16 @@ def themeUp(string):
 
 MESSAGES = {
     "added-title": "\n {}\n".format(themeUp("{normal}Added files:")),
-    "branch": " {} {}".format(themeUp("{normal}Branch:"), themeUp("{keyword}$")),
+    "branch": " {} {}".format(themeUp("{normal}⚲ Branch:"), themeUp("{keyword}$")),
     "change": "\t{}".format(themeUp("{success}$")),
-    "clean": "\n {}\n {}".format(themeUp("{normal}Congratulations!"), themeUp("{success}You are clean!")),
+    "clean": "\n {}\n {}".format(themeUp("{normal}⚑ Congratulations!"), themeUp("{success}⚑ You are clean!")),
     "deleted-title": "\n {}\n".format(themeUp("{normal}Deleted files:")),
     "modified-title": "\n {}\n".format(themeUp("{normal}Modified files:")),
     "renamed-title": "\n {}\n".format(themeUp("{normal}Renamed files:")),
     "untracked-title": "\n {}\n".format(themeUp("{normal}Untracked files:")),
-    "deleted": "\t{}".format(themeUp("{normal}$")),
-    "modified": "\t{}".format(themeUp("{modified}$")),
-    "renamed": "\t{}".format(themeUp("{renamed}$")),
-    "untracked": "\t{}".format(themeUp("{untracked}$")),
-    "added": "\t{}".format(themeUp("{added}$")),
+    "deleted": "\t{}".format(themeUp("{deleted}✝ $")),
+    "modified": "\t{}".format(themeUp("{modified}☢ $")),
+    "renamed": "\t{}".format(themeUp("{renamed}✦ $")),
+    "untracked": "\t{}".format(themeUp("{untracked}✱ $")),
+    "added": "\t{}".format(themeUp("{added}✚ $")),
 }
