0cd2a769dd6ca4958175e69b1de8385501b28f43
Luis Olmedo <olmedoluis012@gmail.com>
2020-05-09 17:46:08 -0300

    refactor(Pix.py): Pass messages in router

--- src/scripts/Pix.py
+++ src/scripts/Pix.py
@@ -35,6 +35,7 @@ class mainRouter():
         self.user_routes = argv
         self.actual_route = argv[0]
         self.leftKeys = argv[1:]
+        self.messages = messages
 
     def runAgain(self, router):
         run(router)
