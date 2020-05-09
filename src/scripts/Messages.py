45a86b24abe00cd1ffa768b4a907215bba24be0c
Luis Olmedo <olmedoluis012@gmail.com>
2020-05-08 21:39:03 -0300

    refactor(Messages.py): Improve messages exported data

--- src/scripts/Messages.py
+++ src/scripts/Messages.py
@@ -1,23 +1,26 @@
-from Theme import THEME
+from Theme import getTheme
 
 
-def themeUp(string):
-    return string.format(**THEME).replace("$", "{}") + THEME["reset"]
+def getMessages():
+    theme = getTheme()
 
+    def themeUp(string):
+        return string.format(**theme).replace("$", "{}") + theme["reset"]
 
-MESSAGES = {
-    "added-title": "\n {}\n".format(themeUp("{normal}Added files:")),
-    "added": "\t{}".format(themeUp("{added}✚ $")),
-    "branch": " {} {}".format(themeUp("{normal}⚲ Branch:"), themeUp("{keyword}$")),
-    "change": "\t{}".format(themeUp("{success}$")),
-    "clean": "\n {}\n {}".format(themeUp("{normal}⚑ Congratulations!"), themeUp("{success}⚑ You are clean!")),
-    "deleted-title": "\n {}\n".format(themeUp("{normal}Deleted files:")),
-    "deleted": "\t{}".format(themeUp("{deleted}✝ $")),
-    "modified-title": "\n {}\n".format(themeUp("{normal}Modified files:")),
-    "modified": "\t{}".format(themeUp("{modified}☢ $")),
-    "renamed-modify": "{}".format(themeUp("$/{modified}$")),
-    "renamed-title": "\n {}\n".format(themeUp("{normal}Renamed files:")),
-    "renamed": "\t{}".format(themeUp("{renamed}✦ $")),
-    "untracked-title": "\n {}\n".format(themeUp("{normal}Untracked files:")),
-    "untracked": "\t{}".format(themeUp("{untracked}✱ $")),
-}
+    return {
+        "added-title": "\n {}\n".format(themeUp("{normal}Added files:")),
+        "added": "\t{}".format(themeUp("{added}✚ $")),
+        "branch": " {} {}".format(themeUp("{normal}⚲ Branch:"), themeUp("{keyword}$")),
+        "change": "\t{}".format(themeUp("{success}$")),
+        "clean": "\n {}\n {}".format(themeUp("{normal}⚑ Congratulations!"), themeUp("{success}⚑ You are clean!")),
+        "deleted-title": "\n {}\n".format(themeUp("{normal}Deleted files:")),
+        "deleted": "\t{}".format(themeUp("{deleted}✝ $")),
+        "modified-title": "\n {}\n".format(themeUp("{normal}Modified files:")),
+        "modified": "\t{}".format(themeUp("{modified}☢ $")),
+        "renamed-modify": "{}".format(themeUp("$/{modified}$")),
+        "renamed-title": "\n {}\n".format(themeUp("{normal}Renamed files:")),
+        "renamed": "\t{}".format(themeUp("{renamed}✦ $")),
+        "untracked-title": "\n {}\n".format(themeUp("{normal}Untracked files:")),
+        "untracked": "\t{}".format(themeUp("{untracked}✱ $")),
+        "unknownRoute": "\t{}".format(themeUp("{success}${normal}$")),
+    }
