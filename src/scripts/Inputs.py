1c7f4bf8f0d4bf5afb4af299382544e5128f3c0e
Luis Olmedo <olmedoluis012@gmail.com>
2020-05-12 00:03:53 -0300

    refactor(Inputs.py): Add multi text logic

--- src/scripts/Inputs.py
+++ src/scripts/Inputs.py
@@ -12,17 +12,21 @@ def prompts(data):
     if not "title" in data:
         data["title"] = ""
 
-    def setup():
-        title = data["title"]
+    def text_validator(word, char):
+        if "\n" in char:
+            return False
+        elif char == "KEY_BACKSPACE":
+            return word[:-1]
+        else:
+            word += char
+            return word
 
-        stdscr.addstr(1, 0, f" {title}")
-
-    def text():
+    def text(validator, prefix="", root=3, postfix="", placeholder="", title=""):
         global word, last_word
+        word = ""
+        last_word = ""
 
         def getDisplayString(word):
-            placeholder = data["placeholder"]
-
             if word == "":
                 return placeholder
 
@@ -30,10 +34,15 @@ def prompts(data):
 
         while True:
             stdscr.clear()
-            setup()
+
+            stdscr.addstr(1, 1, f"{title}")
 
             user_input = getDisplayString(word)
-            stdscr.addstr(3, 0, f"\t{user_input}")
+
+            formated_user_input = f"{prefix}{user_input}{postfix}"
+            stdscr.addstr(4, root, formated_user_input)
+            stdscr.addstr(4, root, f"{prefix}{user_input}")
+
             stdscr.refresh()
 
             last_word += word
@@ -43,41 +52,70 @@ def prompts(data):
             except:
                 break
 
-            if "\n" in char:
-                break
-            elif char == "KEY_BACKSPACE":
-                word = word[:-1]
-            else:
-                word += char
+            validation = validator(word, char)
 
-    def multitext():
-        print("multiselect")
-        sleep(5)
+            if validation == False:
+                break
+
+            word = validation
+
+        return word if word != "" else placeholder
+
+    def multitext(validator):
+        template = data["template"]
+
+        names = []
+        children = data["children"]
+
+        for child in children:
+            names.append(child["name"])
+            if not "placeholder" in child:
+                child["placeholder"] = child["name"]
+
+            if not "title" in child:
+                child["title"] = ""
+
+        for index in range(0, len(names)):
+            child = children[index]
+            prefix, postfix = template.replace("{}", "$", 1).split("$", 1)
+            postfix = postfix.format(*names[index + 1:])
+
+            value = text(text_validator, prefix=prefix,
+                         postfix=postfix, placeholder=child["placeholder"], title=child["title"])
+
+            template = template.replace("{}", value, 1)
+
+        return template
 
     prompts_store = {
-        "text": text,
-        "multitext": multitext
+        "text": {"prompt": text, "validator": text_validator},
+        "multitext": {"prompt": multitext, "validator": text_validator}
     }
 
-    actual_prompt = prompts_store[data["type"]]
+    selectedPrompt = prompts_store[data["type"]]
 
     stdscr = curses.initscr()
     curses.cbreak()
     stdscr.keypad(True)
-
+    result = ""
     try:
-        actual_prompt()
+        actual_prompt = selectedPrompt["prompt"]
+        validator = selectedPrompt["validator"]
+        result = actual_prompt(validator)
     finally:
         curses.nocbreak()
         stdscr.keypad(False)
         curses.echo()
         curses.endwin()
+        print(result)
 
 
 prompts({
-    "type": "text",
+    "type": "multitext",
+    "title": "que ondis?",
     "placeholder": "weon",
-    "title": "que onda weon?"
+    "children": [{"name": "type", "placeholder": "feat", "title": "escribi el tipo:"}, {"name": "type", "placeholder": "feat", "title": "escribi el scope:"}, {"name": "about"}],
+    "template": "{}({}):{}"
 })
 
 exit()
