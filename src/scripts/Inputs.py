7d49b7630cb12153c7fc935e3621da27656e35b3
Luis Olmedo <olmedoluis012@gmail.com>
2020-05-23 01:26:30 -0300

    feat(Inputs.py): Add multitext logic

--- src/scripts/Inputs.py
+++ src/scripts/Inputs.py
@@ -11,13 +11,26 @@ from ConsoleControl import console
 #     "template": "{}({}):{}"
 # })
 
+def removeCursor(string):
+    return string.replace("$C", "")
+
+
+def addCursor(string):
+    return string + "$C"
+
+
+def updateCursor(string, stringToAdd):
+    return string.replace("$C", "") + stringToAdd + "$C"
+
+
 class prompt():
-    def __init__(self, variety, title="", placeHolder="", children=[], template=""):
+    def __init__(self, variety, title="", placeHolder="", children=[], template="", content=""):
         self.variety = variety
         self.title = title
         self.placeHolder = placeHolder
         self.children = children
         self.template = template
+        self.content = content
 
 
 def simpleTextInput(data):
@@ -41,21 +54,95 @@ def simpleTextInput(data):
     return word if word != "" else placeHolder
 
 
+class ShownValue():
+    def __init__(self, placeHolder):
+        self.placeHolder = placeHolder
+        self.showPlaceHolder = True
+
+    def getValue(self, string):
+        if self.showPlaceHolder == True and removeCursor(string) == "":
+            return self.placeHolder
+
+        elif self.showPlaceHolder == False and removeCursor(string) == "":
+            self.showPlaceHolder = True
+            return self.placeHolder
+
+        elif self.showPlaceHolder == True and removeCursor(string) == self.placeHolder:
+            self.showPlaceHolder = False
+            return self.placeHolder
+
+        output = string.replace(
+            self.placeHolder, "") if self.showPlaceHolder == True else addCursor(string)
+        return output
+
+
+def multiTextInput(data):
+    word = ""
+    currentPrompt = prompt(**data)
+    children = currentPrompt.children
+    index = 0
+    childrenAvailable = len(children)
+
+    childrenContents = []
+    childrenPlaceHolders = []
+    for child in children:
+        childPrompt = prompt(**child)
+        defaultValue = ShownValue(childPrompt.placeHolder)
+        childrenPlaceHolders.append(defaultValue)
+
+        initValue = defaultValue.getValue(childPrompt.content)
+
+        childrenContents.append(initValue)
+    childrenContents[0] = addCursor(childrenContents[0])
+
+    print()
+    while True:
+        indexInRange = index % childrenAvailable
+        child = children[indexInRange]
+        childContent = childrenContents[indexInRange]
+        childPlaceHolder = childrenPlaceHolders[indexInRange]
+        childPrompt = prompt(**child)
+        title = childPrompt.title
+
+        formatWord = currentPrompt.template.format(*childrenContents)
+        myTerm.show([title, "", formatWord])
+        myTerm.refresh()
+        char = getch.getch()
+
+        if char == "\n":
+            index = index + 1
+            childrenContents[indexInRange] = removeCursor(childContent)
+            if indexInRange == childrenAvailable - 1:
+                break
+            continue
+        # elif not char.isalpha():
+        #     break
+
+        childrenContents[indexInRange] = childPlaceHolder.getValue(
+            updateCursor(childContent, char))
+    print()
+
+    return word if word != "" else currentPrompt.template.format(*childrenContents)
+
+
 myTerm = console(0)
 
 try:
-    simpleTextInput({
+    wea = multiTextInput({
         "variety": "multitext",
         "title": "que ondis?",
-        "placeHolder": "weon"
+        "placeHolder": "weon",
+        "children": [{"variety": "type", "placeHolder": "feat", "title": "escribi el tipo:"}, {"variety": "type", "placeHolder": "scope", "title": "tira el scope:"}, {"variety": "type", "title": "escribi la descri:"}],
+        "template": "{0}({1}):{2}"
     })
 finally:
     myTerm.finish()
+print(wea)
 exit()
 
 
 # simpleTextInput({
-#     "variety": "multitext",
+#     "variety": "single-text",
 #     "title": "que ondis?",
 #     "placeHolder": "weon"
 # })
