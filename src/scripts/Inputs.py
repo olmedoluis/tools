000dcfc2a75dcfe591e22adf52baba6074b7ba2c
Luis Olmedo <olmedoluis012@gmail.com>
2020-05-25 00:17:19 -0300

    feat(Inputs.py): Add multiselectinput

--- src/scripts/Inputs.py
+++ src/scripts/Inputs.py
@@ -49,6 +49,41 @@ def getResponse(char):
         return "UNKNOWN"
 
 
+def getOptionsWithStates(options):
+    class opt():
+        def __init__(self, content):
+            self.state = False
+            self.content = content
+
+        def getStateString(self):
+            return "+" if self.state else "-"
+
+        def setState(self, newState):
+            self.state = newState
+
+    output = []
+    for option in options:
+        output.append(opt(option))
+
+    return output
+
+
+def getOptionsSelected(options):
+    output = []
+    for option in options:
+        if option.state:
+            output.append(option)
+    return output
+
+
+def getOptionContents(options):
+    output = []
+    for option in options:
+        if option.state:
+            output.append(option.content)
+    return output
+
+
 def getIndexValue(array, index, defaultValue=""):
     return array[index] if index < len(array) else defaultValue
 
@@ -159,10 +194,65 @@ def confirmInput(title="", content="", finalTitle="", errorMessage=""):
     return word
 
 
+def multiSelectInput(title="", finalTitle="", options=[""], errorMessage=""):
+    inputConsole = console(5)
+    finalTitle = finalTitle if finalTitle != "" else title
+    index = 0
+    optionsLen = len(options)
+    optionsWithStates = getOptionsWithStates(options)
+
+    inputConsole.setConsoleLine(0, 1, title)
+
+    while True:
+        optionAbove = optionsWithStates[(index - 1) % optionsLen]
+        optionSelected = optionsWithStates[index % optionsLen]
+        optionDown = optionsWithStates[(index + 1) % optionsLen]
+
+        inputConsole.setConsoleLine(
+            2, 4, f"{optionAbove.getStateString()} {optionAbove.content}")
+
+        inputConsole.setConsoleLine(
+            3, 4, f"{optionSelected.getStateString()} {optionSelected.content}")
+
+        inputConsole.setConsoleLine(
+            4, 4, f"{optionDown.getStateString()} {optionDown.content}")
+
+        inputConsole.refresh()
+
+        char = getch.getch()
+        state = getMovement(char)
+
+        if state == "DOWN":
+            index = index + 1
+        elif state == "UP":
+            index = index - 1
+        elif state == "RIGHT":
+            optionSelected.setState(True)
+        elif state == "LEFT":
+            optionSelected.setState(False)
+        elif state == "FINISH":
+            break
+        elif state == "BREAK_CHAR":
+            print(errorMessage)
+            exit()
+
+    selectedOptions = getOptionsSelected(optionsWithStates)
+    selectedOptionsString = ", ".join(getOptionContents(selectedOptions))
+
+    inputConsole.setConsoleLine(0, 1, f"{finalTitle} {selectedOptionsString}")
+    inputConsole.refresh()
+    inputConsole.deleteLastLines(4)
+
+    inputConsole.finish()
+
+    return selectedOptions
+
+
 options = ["feature", "refactor", "bugfix", "style"]
+output = multiSelectInput(title="me das la data gil?", options=options)
 # output = selectInput(title="me das la data gil?", options=options)
 # output = confirmInput(title="DECIMELO Y/N")
-output = textInput(title="algo", placeHolder="answer")
+# output = textInput(title="algo", placeHolder="answer")
 print("output", output)
 
 exit()
