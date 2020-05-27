967f413798b4cfbdf1fb9552a3a6b34891bc6c4a
Luis Olmedo <olmedoluis012@gmail.com>
2020-05-27 00:20:06 -0300

    feat(Messages.py): Add messages for commiting

--- src/scripts/Data/Messages.py
+++ src/scripts/Data/Messages.py
@@ -62,4 +62,13 @@ def getMessages():
         "add-adition-notafile": "\n {}\n".format(themeUp("{error}✖ One of the files you input is not a file")),
         "add-all-nofiles": "\n {}\n".format(themeUp("{error}✖ There is no file to add")),
         "add-all-success": "\n {}\n".format(themeUp("{success}⚑ All files has been added")),
+        "commit-type-title": "{}".format(themeUp("{normal}What type of commit is this?")),
+        "commit-scope-title": "{}".format(themeUp("{normal}What is the scope?")),
+        "commit-about-title": "{}".format(themeUp("{normal}What is this commit about?")),
+        "commit-preview": "\n\t{}\n".format(themeUp("{normal}Preview: {keyword}$")),
+        "commit-confirm": "{}".format(themeUp("{normal}Are you sure? y/n")),
+        "commit-success": "\n {}\n".format(themeUp("{success}⚑ You have commited added files")),
+        "commit-cancel": "\n {}\n".format(themeUp("{error}✖ You have cancelled commiting")),
+        "commit-empty": "\n {}\n".format(themeUp("{error}✖ You left a field empty")),
+        "scape-error": "\n {}\n".format(themeUp("{error}✖ You have exit pix")),
     }
