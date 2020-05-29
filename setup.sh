d9491c4c899a304ef5fc8a2fe2718714dbc3951b
Luis Olmedo <olmedoluis012@gmail.com>
2020-05-28 23:15:09 -0300

    refactor(setup.sh):Properly set up shortcuts

--- setup.sh
+++ setup.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
-chmod +x ./pix.sh
+# Get current file directory
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+CURRENTDIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
 
-alias pxb="/home/luis/Documents/Projects/pix-bash"
+PIXDIR="$CURRENTDIR/scripts/Pix.py"
+PIXALIAS="alias pxb=$PIXDIR"
+
+echo
+# Enable to execute pix program
+[ ! -x $PIXDIR ] && echo " Enable to execute" && chmod +x $PIXDIR
+
+# Set up command shortcut for bash
+BASH=~/.bash_aliases
+if test -f "$BASH" && ! cat ~/.bash_aliases | grep "alias pxb=$PIXDIR" &>/dev/null; then
+  echo " Setting up bash shortcut.."
+  echo $PIXALIAS >>${BASH}
+fi
+
+# Set up command shortcut for ZSH
+ZSH=~/.zshrc
+if test -f "$ZSH" && ! cat ~/.zshrc | grep "alias pxb=$PIXDIR" &>/dev/null; then
+  echo " Setting up zsh shortcut.."
+  echo $PIXALIAS >>${ZSH}
+  source $ZSH &>/dev/null
+  echo
+fi
+
+echo " Pix has been installed!"
+echo
