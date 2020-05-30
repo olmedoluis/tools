54ecddf85e383a62c60f63b3eee88a303d0c9d36
Luis Olmedo <olmedoluis012@gmail.com>
2020-05-30 17:06:20 -0300

    refactor(Commit.py):Update renamed message

--- scripts/Commit.py
+++ scripts/Commit.py
@@ -64,7 +64,7 @@ def save():
                             {"type": "Text", "title": messages["commit-about-title"]}])
 
     if len(answers) != 3:
-        return print(messages["commit-empty"])
+        return print(messages["error-empty"])
 
     commit = "{}({}):{}".format(*answers)
     print(messages["commit-preview"].format(commit))
