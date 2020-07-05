6393b49a9c31642df32032428adab983057a4b57
Luis Olmedo <olmedoluis012@gmail.com>
2020-07-05 14:01:51 -0300

    feat(__main__.py):Add posibility to concatenate commands

--- Pix/__main__.py
+++ Pix/__main__.py
@@ -36,35 +36,53 @@ class PixTools:
         return "" if 0 == len(self.leftKeys) else self.leftKeys[0]
 
 
+def getConcatenatedRoutes(routes):
+    indexes = [i for i, e in enumerate(routes) if e == "n"]
+    lastRoute = routes[indexes[-1] + 1 :]
+    allRoutes = []
+    lastIndex = 0
+
+    for index in indexes:
+        allRoutes.append(routes[lastIndex:index])
+        lastIndex = index + 1
+
+    allRoutes.append(lastRoute)
+
+    return allRoutes
+
+
 def main():
     arg = sys.argv[1:]
 
     if len(arg) == 0:
         return
 
-    pixTools = PixTools(arg)
+    allRoutes = getConcatenatedRoutes(arg) if "n" in arg else [arg]
 
-    route_name = checkPixShortcut(pixTools.actual_route, ROUTES)
+    for arguments in allRoutes:
+        pixTools = PixTools(arguments)
 
-    if route_name == False:
-        good_routes = f"pix {' '.join(pixTools.getGoodRoutes())}"
-        wrong_routes = f"{pixTools.actual_route} {' '.join(pixTools.leftKeys)}"
-        return print(messages["unknownRoute"].format(good_routes, wrong_routes))
+        route_name = checkPixShortcut(pixTools.actual_route, ROUTES)
 
-    PIX_STORE = {
-        "Status": StatusRouter,
-        "Add": AddRouter,
-        "Remove": RemoveRouter,
-        "Commit": CommitRouter,
-        "Branch": BranchRouter,
-        "Stash": StashRouter,
-    }
+        if route_name == False:
+            good_routes = f"pix {' '.join(pixTools.getGoodRoutes())}"
+            wrong_routes = f"{pixTools.actual_route} {' '.join(pixTools.leftKeys)}"
+            return print(messages["unknownRoute"].format(good_routes, wrong_routes))
 
-    next_route = pixTools.getNextRoute()
-    subroute = checkRoute(next_route, SUBROUTES[route_name])
+        PIX_STORE = {
+            "Status": StatusRouter,
+            "Add": AddRouter,
+            "Remove": RemoveRouter,
+            "Commit": CommitRouter,
+            "Branch": BranchRouter,
+            "Stash": StashRouter,
+        }
 
-    requiredRouter = PIX_STORE[route_name]
-    requiredRouter(pixTools, subroute)
+        next_route = pixTools.getNextRoute()
+        subroute = checkRoute(next_route, SUBROUTES[route_name])
+
+        requiredRouter = PIX_STORE[route_name]
+        requiredRouter(pixTools, subroute)
 
 
 if __name__ == "__main__":
