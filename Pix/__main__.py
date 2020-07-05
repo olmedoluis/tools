c6e2e96f831333fe82b64d62d53a1b09bba611db
olmedoluis <63429352+olmedoluis@users.noreply.github.com>
2020-07-05 15:21:41 -0300

    [PIX-27] Add posibility to concatenate commands (#18)
    * feat(__main__.py):Add posibility to concatenate commands
    
    * refactor(__main__.py):Declare repeated variables outside

--- Pix/__main__.py
+++ Pix/__main__.py
@@ -36,20 +36,26 @@ class PixTools:
         return "" if 0 == len(self.leftKeys) else self.leftKeys[0]
 
 
+def getConcatenatedRoutes(routes):
+    indexes = [i for i, e in enumerate(routes) if e == "n"]
+    lastRoute = routes[indexes[-1] + 1 :]
+    allRoutes = []
+    lastIndex = 0
+
+    for index in indexes:
+        allRoutes.append(routes[lastIndex:index])
+        lastIndex = index + 1
+
+    allRoutes.append(lastRoute)
+
+    return allRoutes
+
+
 def main():
     arg = sys.argv[1:]
 
     if len(arg) == 0:
         return
-
-    pixTools = PixTools(arg)
-
-    route_name = checkPixShortcut(pixTools.actual_route, ROUTES)
-
-    if route_name == False:
-        good_routes = f"pix {' '.join(pixTools.getGoodRoutes())}"
-        wrong_routes = f"{pixTools.actual_route} {' '.join(pixTools.leftKeys)}"
-        return print(messages["unknownRoute"].format(good_routes, wrong_routes))
 
     PIX_STORE = {
         "Status": StatusRouter,
@@ -60,11 +66,23 @@ def main():
         "Stash": StashRouter,
     }
 
-    next_route = pixTools.getNextRoute()
-    subroute = checkRoute(next_route, SUBROUTES[route_name])
+    allRoutes = getConcatenatedRoutes(arg) if "n" in arg else [arg]
 
-    requiredRouter = PIX_STORE[route_name]
-    requiredRouter(pixTools, subroute)
+    for arguments in allRoutes:
+        pixTools = PixTools(arguments)
+
+        route_name = checkPixShortcut(pixTools.actual_route, ROUTES)
+
+        if route_name == False:
+            good_routes = f"pix {' '.join(pixTools.getGoodRoutes())}"
+            wrong_routes = f"{pixTools.actual_route} {' '.join(pixTools.leftKeys)}"
+            return print(messages["unknownRoute"].format(good_routes, wrong_routes))
+
+        next_route = pixTools.getNextRoute()
+        subroute = checkRoute(next_route, SUBROUTES[route_name])
+
+        requiredRouter = PIX_STORE[route_name]
+        requiredRouter(pixTools, subroute)
 
 
 if __name__ == "__main__":
