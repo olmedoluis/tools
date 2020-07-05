d7da1ef7547347509e3f1b808eb34de6afa6a8f2
Luis Olmedo <olmedoluis012@gmail.com>
2020-07-05 01:31:15 -0300

    refactor(__main__.py):Improve main function

--- Pix/__main__.py
+++ Pix/__main__.py
@@ -14,34 +14,6 @@ from .Modules.Helpers import checkPixShortcut, checkRoute
 messages = getMessages()
 
 
-def Router(route_name, pixTools):
-    PIX_STORE = {
-        "Status": StatusRouter,
-        "Add": AddRouter,
-        "Remove": RemoveRouter,
-        "Commit": CommitRouter,
-        "Branch": BranchRouter,
-        "Stash": StashRouter,
-    }
-
-    next_route = pixTools.getNextRoute()
-    subroute = checkRoute(next_route, SUBROUTES[route_name])
-    requiredRouter = PIX_STORE[route_name]
-
-    requiredRouter(pixTools, subroute)
-
-
-def run(pixTools):
-    route_name = checkPixShortcut(pixTools.actual_route, ROUTES)
-
-    if route_name != False:
-        Router(route_name, pixTools)
-    else:
-        good_routes = f"pix {' '.join(pixTools.getGoodRoutes())}"
-        wrong_routes = f"{pixTools.actual_route} {' '.join(pixTools.leftKeys)}"
-        print(messages["unknownRoute"].format(good_routes, wrong_routes))
-
-
 class PixTools:
     def __init__(self, argv):
         self.user_routes = argv
@@ -67,9 +39,32 @@ class PixTools:
 def main():
     arg = sys.argv[1:]
 
-    if len(arg) != 0:
-        pixTools = PixTools(arg)
-        run(pixTools)
+    if len(arg) == 0:
+        return
+
+    pixTools = PixTools(arg)
+
+    route_name = checkPixShortcut(pixTools.actual_route, ROUTES)
+
+    if route_name == False:
+        good_routes = f"pix {' '.join(pixTools.getGoodRoutes())}"
+        wrong_routes = f"{pixTools.actual_route} {' '.join(pixTools.leftKeys)}"
+        return print(messages["unknownRoute"].format(good_routes, wrong_routes))
+
+    PIX_STORE = {
+        "Status": StatusRouter,
+        "Add": AddRouter,
+        "Remove": RemoveRouter,
+        "Commit": CommitRouter,
+        "Branch": BranchRouter,
+        "Stash": StashRouter,
+    }
+
+    next_route = pixTools.getNextRoute()
+    subroute = checkRoute(next_route, SUBROUTES[route_name])
+    requiredRouter = PIX_STORE[route_name]
+
+    requiredRouter(pixTools, subroute)
 
 
 if __name__ == "__main__":
