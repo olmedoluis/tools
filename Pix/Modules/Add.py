7f3d6d68e7487e64b634a295a5c23a372eee3328
Luis Olmedo <olmedoluis012@gmail.com>
2020-07-06 20:27:57 -0300

    refactor(Add.py):Implement add search by include

--- Pix/Modules/Add.py
+++ Pix/Modules/Add.py
@@ -4,33 +4,34 @@ from .Inputs import prompts
 from pathlib import Path as isFile
 
 
-def add(filePaths=[]):
-    specificFiles = []
-    for filePath in filePaths:
-        if not isFile(filePath):
-            break
-
-        specificFiles.append(filePath)
-
-    if len(specificFiles) > 0:
-        run(["git", "add"] + specificFiles)
-        return print(messages["add-success"])
-
+def add(filePaths=[], shouldVerify=True):
     status = getStatus()
+    inputs = prompts()
 
-    options = []
-    for statusId in status:
-        statusContent = status[statusId]
-        if statusId == "branch" or statusId == "added":
-            continue
+    specificFiles = []
+    if len(filePaths) != 0 and shouldVerify:
+        specificFiles = searchInStatus(
+            filePaths, status, excludedFiles=["branch", "added"]
+        )
 
-        options = options + statusContent
+    if len(specificFiles) == 1:
+        run(["git", "add"] + specificFiles)
+        return print(messages["remove-success"])
+
+    options = specificFiles
+    if len(specificFiles) == 0:
+        for statusId in status:
+            statusContent = status[statusId]
+            if statusId == "branch" or statusId == "added":
+                continue
+
+            options = options + statusContent
 
     if len(options) == 0:
         return print(messages["add-nofiles-error"])
 
     print()
-    answers = prompts().multiSelect(
+    answers = inputs.multiSelect(
         title=messages["add-adition-title"],
         finalTitle=messages["file-selection-finaltitle"],
         options=options,
