879608554836073b8cbcaf41e5d58c8356976a70
Luis Olmedo <olmedoluis012@gmail.com>
2020-07-09 19:22:51 -0300

    feat(InputBookSelection.py):Add posibility to scroll beetwen lines

--- Pix/Modules/Prompts/InputBookSelection.py
+++ Pix/Modules/Prompts/InputBookSelection.py
@@ -1,3 +1,41 @@
-def bookSelection(text=""):
-    return text
+def bookSelection(lines=[]):
+    from .Console import ConsoleControl, getGetch
+    from .CharactersInterpreter import getMovement
+    import os
+
+    _, terminalWidth = os.popen("stty size", "r").read().split()
+
+    getch = getGetch()
+    inputConsole = ConsoleControl(10)
+
+    offset = 0
+
+    def updateConsole():
+        for lineNumber in range(0, 10):
+            lineText = lines[lineNumber + offset].strip()
+
+            inputConsole.setConsoleLine(lineNumber, 1, lineText[0 : int(terminalWidth) - 1])
+
+        inputConsole.refresh()
+
+    while True:
+        updateConsole()
+
+        char = getch()
+        state = getMovement(char)
+
+        if state == "DOWN":
+            offset = offset + 1
+        elif state == "UP":
+            offset = offset - 1
+        elif state == "FINISH" or state == "RIGHT":
+            break
+        elif state == "BREAK_CHAR":
+            # print(errorMessage)
+            inputConsole.deleteLastLines(3)
+            exit()
+
+    inputConsole.finish()
+
+    return ""
 
