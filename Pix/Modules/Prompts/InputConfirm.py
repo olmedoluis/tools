443138e8edada54855fa650b8506b95c6cf44b7a
Luis Olmedo <olmedoluis012@gmail.com>
2020-07-09 17:25:23 -0300

    refactor(InputConfirm.py):Separate bool input into file

---
+++ Pix/Modules/Prompts/InputConfirm.py
@@ -0,0 +1,37 @@
+def confirmInput(title="", finalTitle=""):
+    from .Console import ConsoleControl, getGetch
+    from .CharactersInterpreter import getResponse
+
+    getch = getGetch()
+    inputConsole = ConsoleControl(1)
+
+    word = False
+    try:
+        finalTitle = finalTitle if finalTitle != "" else title
+
+        while True:
+            inputConsole.setConsoleLine(0, 1, f"{title}")
+            inputConsole.refresh()
+
+            char = getch()
+            state = getResponse(char)
+
+            if state == "YES":
+                word = True
+                break
+            if state == "NO":
+                word = False
+                break
+            if state == "FINISH":
+                break
+            if state == "BREAK_CHAR":
+                exit()
+
+        inputConsole.setConsoleLine(0, 1, f"{finalTitle} {word}")
+        inputConsole.refresh()
+
+    finally:
+        inputConsole.finish()
+
+        return word
+
