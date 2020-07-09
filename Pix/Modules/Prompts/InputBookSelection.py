d02c7be75b30c8b7b607a8a64bc5a93aa1fef357
Luis Olmedo <olmedoluis012@gmail.com>
2020-07-09 19:42:56 -0300

    refactor(InputBookSelection.py):limit scrolling

--- Pix/Modules/Prompts/InputBookSelection.py
+++ Pix/Modules/Prompts/InputBookSelection.py
@@ -1,4 +1,4 @@
-def bookSelection(lines=[]):
+def bookSelection(lines=[], seleccionableLinesIncludes=[]):
     from .Console import ConsoleControl, getGetch
     from .CharactersInterpreter import getMovement
     import os
@@ -6,19 +6,19 @@ def bookSelection(lines=[]):
     _, terminalWidth = os.popen("stty size", "r").read().split()
 
     getch = getGetch()
-    inputConsole = ConsoleControl(10)
-    maxLines = len(lines)
+    inputConsole = ConsoleControl(9)
+    textZoneArea = range(0, len(lines))
+    selectionableArea = range(-3, len(lines) - 3)
 
     offset = 0
 
     def updateConsole():
-        for lineNumber in range(0, 10):
+        for lineNumber in range(0, 9):
             index = lineNumber + offset
-            lineText = lines[index].strip() if index < maxLines and index > 0 else ""
+            lineText = lines[index].strip() if index in textZoneArea else ""
+            lineTextLimited = lineText[0 : int(terminalWidth) - 1]
 
-            inputConsole.setConsoleLine(
-                lineNumber, 1, lineText[0 : int(terminalWidth) - 1]
-            )
+            inputConsole.setConsoleLine(lineNumber, 1, lineTextLimited)
 
         inputConsole.refresh()
 
@@ -29,14 +29,16 @@ def bookSelection(lines=[]):
         state = getMovement(char)
 
         if state == "DOWN":
-            offset = offset + 1
+            newOffset = offset + 1
+            offset = newOffset if newOffset in selectionableArea else offset
         elif state == "UP":
-            offset = offset - 1
+            newOffset = offset - 1
+            offset = newOffset if newOffset in selectionableArea else offset
         elif state == "FINISH" or state == "RIGHT":
             break
         elif state == "BREAK_CHAR":
             # print(errorMessage)
-            inputConsole.deleteLastLines(3)
+            # inputConsole.deleteLastLines(3)
             exit()
 
     inputConsole.finish()
