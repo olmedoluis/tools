6f92fb74ddd32b64f60fbd8cf3bdaa5a59f5894a
Luis Olmedo <olmedoluis012@gmail.com>
2020-07-09 15:39:18 -0300

    refactor(Console.py):Improve imports

--- Pix/Modules/Prompts/Console.py
+++ Pix/Modules/Prompts/Console.py
@@ -1,78 +1,83 @@
-def console(height):
-    import sys
-    import os
+from sys import stdout
+from os import name as osName
 
-    if os.name == "nt":
-        import msvcrt
-        import ctypes
+if osName == "nt":
+    import ctypes
 
-        class _CursorInfo(ctypes.Structure):
-            _fields_ = [("size", ctypes.c_int), ("visible", ctypes.c_byte)]
+    class _CursorInfo(ctypes.Structure):
+        _fields_ = [("size", ctypes.c_int), ("visible", ctypes.c_byte)]
 
-    def hide_cursor():
-        if os.name == "nt":
-            ci = _CursorInfo()
-            handle = ctypes.windll.kernel32.GetStdHandle(-11)
-            ctypes.windll.kernel32.GetConsoleCursorInfo(handle, ctypes.byref(ci))
-            ci.visible = False
-            ctypes.windll.kernel32.SetConsoleCursorInfo(handle, ctypes.byref(ci))
-        elif os.name == "posix":
-            sys.stdout.write("\033[?25l")
-            sys.stdout.flush()
 
-    def show_cursor():
-        if os.name == "nt":
-            ci = _CursorInfo()
-            handle = ctypes.windll.kernel32.GetStdHandle(-11)
-            ctypes.windll.kernel32.GetConsoleCursorInfo(handle, ctypes.byref(ci))
-            ci.visible = True
-            ctypes.windll.kernel32.SetConsoleCursorInfo(handle, ctypes.byref(ci))
-        elif os.name == "posix":
-            sys.stdout.write("\033[?25h")
-            sys.stdout.flush()
+def hide_cursor():
+    if osName == "nt":
+        ci = _CursorInfo()
+        handle = ctypes.windll.kernel32.GetStdHandle(-11)
+        ctypes.windll.kernel32.GetConsoleCursorInfo(handle, ctypes.byref(ci))
+        ci.visible = False
+        ctypes.windll.kernel32.SetConsoleCursorInfo(handle, ctypes.byref(ci))
+    elif osName == "posix":
+        stdout.write("\033[?25l")
+        stdout.flush()
 
-    code = "\x1b["
-    log = sys.stdout.write
 
-    def cursorUp(times):
-        log(f"{code}{times}A")
+def show_cursor():
+    if osName == "nt":
+        ci = _CursorInfo()
+        handle = ctypes.windll.kernel32.GetStdHandle(-11)
+        ctypes.windll.kernel32.GetConsoleCursorInfo(handle, ctypes.byref(ci))
+        ci.visible = True
+        ctypes.windll.kernel32.SetConsoleCursorInfo(handle, ctypes.byref(ci))
+    elif osName == "posix":
+        stdout.write("\033[?25h")
+        stdout.flush()
 
-    def cleanLine():
-        log(f"{code}2K")
 
-    class term:
-        def __init__(self, lines):
-            self.display = (" " * (lines - 1)).split(" ")
+code = "\x1b["
+log = stdout.write
 
-            for emptyValue in self.display:
-                print(emptyValue)
 
-        def getCode(self, string):
-            return f"\x1b[{string}"
+def cursorUp(times):
+    log(f"{code}{times}A")
 
-        def show(self, data):
-            self.display = data
 
-        def setConsoleLine(self, row=0, column=0, content=""):
-            self.display[row] = " " * column + content
+def cleanLine():
+    log(f"{code}2K")
 
-        def refresh(self):
-            count = len(self.display)
-            cursorUp(count)
-            for line in self.display:
-                cleanLine()
-                print(line.replace("$C", "❚"))
 
-        def deleteLastLines(self, lines):
-            lastNumberOfLines = len(self.display)
-            newNumberOfLines = lastNumberOfLines - lines
-            self.display = (" " * (newNumberOfLines)).split(" ")
-            for i in range(0, lines):
-                cleanLine()
-                cursorUp(1)
+class term:
+    def __init__(self, lines):
+        self.display = (" " * (lines - 1)).split(" ")
 
-        def finish(self):
-            show_cursor()
+        for emptyValue in self.display:
+            print(emptyValue)
 
-    hide_cursor()
-    return term(height)
+    def getCode(self, string):
+        return f"\x1b[{string}"
+
+    def show(self, data):
+        self.display = data
+
+    def setConsoleLine(self, row=0, column=0, content=""):
+        self.display[row] = " " * column + content
+
+    def refresh(self):
+        count = len(self.display)
+        cursorUp(count)
+        for line in self.display:
+            cleanLine()
+            print(line.replace("$C", "❚"))
+
+    def deleteLastLines(self, lines):
+        lastNumberOfLines = len(self.display)
+        newNumberOfLines = lastNumberOfLines - lines
+        self.display = (" " * (newNumberOfLines)).split(" ")
+        for i in range(0, lines):
+            cleanLine()
+            cursorUp(1)
+
+    def finish(self):
+        show_cursor()
+
+
+hide_cursor()
+return term(height)
