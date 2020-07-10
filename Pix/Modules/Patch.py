a2f3282a1f92248c7fba774312ab2d984bcb5217
Luis Olmedo <olmedoluis012@gmail.com>
2020-07-09 23:34:37 -0300

    refactor(Patch.py):Add logic to patch all

--- Pix/Modules/Patch.py
+++ Pix/Modules/Patch.py
@@ -1,5 +1,35 @@
+def addToFile(text, filePath):
+    f = open(filePath, "w+")
+    f.write(text)
+    f.close()
+
+
 def patchAll():
-    print("patching...")
+    from .Helpers import run
+    from pathlib import Path
+    from .Prompts import bookSelection
+
+    cwd = Path.cwd()
+    filePath = f"{cwd}/changes.patch"
+
+    differences = run(["git", "diff"])
+    wea = bookSelection(lines=differences, seleccionableLinesIncludes=["+", "-"])
+    
+    return print(differences)
+    addToFile(differences, filePath)
+    run(
+        [
+            "git",
+            "apply",
+            "--ignore-space-change",
+            "--ignore-whitespace",
+            "--cached",
+            filePath,
+        ]
+    )
+
+    run(["rm", filePath])
+
 
 def setUp(outsideMessages):
     global messages
