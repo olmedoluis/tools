0f9e123cf3c1fe655cde2230963422b7adff8965
Luis Olmedo <olmedoluis012@gmail.com>
2020-07-15 20:56:46 -0300

    refactor(inputPatchSelect.py):Show lines highlighted with no scroll limit

--- Pix/Modules/Prompts/inputPatchSelect.py
+++ Pix/Modules/Prompts/inputPatchSelect.py
@@ -1,60 +1,40 @@
-def patchSelect(lines=[], seleccionableLinesIncludes=[], colors={"": "\x1b[32m"}, errorMessage=""):
+def patchSelect(lines=[], seleccionableLinesIncludes=[], fileName="", errorMessage=""):
     from .Console import ConsoleControl, getGetch
     from .CharactersInterpreter import getMovement
     from os import popen
 
+    print("\n On " + fileName + "\n")
+
     _, terminalWidth = popen("stty size", "r").read().split()
+    terminalWidth = int(terminalWidth) - 1
 
     reset = "\x1b[0m"
     bold = "\x1b[1m"
     dim = "\x1b[2m"
-    addedColor = "\x1b[32m"
-    deletedColor = "\x1b[31m"
+    colors = {"+": "\x1b[32m", "-": "\x1b[31m"}
 
     getch = getGetch()
-    inputConsole = ConsoleControl(9)
+    inputConsole = ConsoleControl(15)
     textZoneArea = range(0, len(lines))
-    selectionableArea = range(-4, len(lines) - 4)
 
-    linesToSelect = []
-    for index in textZoneArea:
-        for selectionableInclusion in seleccionableLinesIncludes:
-            if selectionableInclusion in lines[index]:
-                linesToSelect.append(index - 4)
-
-    offset = linesToSelect[0]
-    indexLimits = range(0, len(linesToSelect))
-    index = 0
-    linesSelected = []
+    offset = 0
+    patchesSelected = []
 
     def updateConsole():
-        for lineNumber in range(0, 9):
+        for lineNumber in range(0, 15):
             index = lineNumber + offset
             lineText = lines[index].strip() if index in textZoneArea else ""
-            lineTextLimited = lineText[0 : int(terminalWidth) - 1]
-            selectionColor = ""
-            if index in linesSelected:
-                option1, option2 = seleccionableLinesIncludes
-                selectionColor = addedColor if lineText[0] == option1 else deletedColor
+            lineTextLimited = lineText[0:terminalWidth]
+            if len(lineText) and lineText[0] in colors:
+                lineTextLimited = colors[lineText[0]] + lineTextLimited + reset
 
-            lineTextColored = (
-                f"{reset}{bold}{selectionColor}{lineTextLimited}"
-                if lineNumber == 4
-                else f"{reset}{dim}{selectionColor}{lineTextLimited}"
-            )
-
-            inputConsole.setConsoleLine(lineNumber, 1, lineTextColored)
+            inputConsole.setConsoleLine(lineNumber, 1, lineTextLimited)
 
         inputConsole.refresh()
 
     def saveLineText(shouldSave, text):
-        symbol = text[0]
-        lineNumber = offset + 4
-        if lineNumber in linesSelected and not shouldSave:
-            index = linesSelected.index(lineNumber)
-            return linesSelected.pop(index)
-        elif not lineNumber in linesSelected and shouldSave:
-            linesSelected.append(lineNumber)
+        return
+        patchesSelected.append(lineNumber)
 
     while True:
         updateConsole()
@@ -63,15 +43,9 @@ def patchSelect(lines=[], seleccionableLinesIncludes=[], colors={"": "\x1b[32m"}
         state = getMovement(char)
 
         if state == "DOWN":
-            newIndex = index + 1
-            if newIndex in indexLimits:
-                offset = linesToSelect[newIndex]
-                index = newIndex
+            offset = offset + 1
         elif state == "UP":
-            newIndex = index - 1
-            if newIndex in indexLimits:
-                offset = linesToSelect[newIndex]
-                index = newIndex
+            offset = offset - 1
         elif state == "RIGHT":
             lineText = lines[offset + 4]
             saveLineText(True, lineText)
@@ -85,9 +59,9 @@ def patchSelect(lines=[], seleccionableLinesIncludes=[], colors={"": "\x1b[32m"}
             inputConsole.finish()
             print(errorMessage)
             exit()
-    
+
     inputConsole.deleteLastLines(9)
     inputConsole.finish()
 
-    return linesSelected
+    return patchesSelected
 
