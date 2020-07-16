be6bd3fff994d7711c548fcfcfaae05d9cd63ac7
Luis Olmedo <olmedoluis012@gmail.com>
2020-07-15 22:19:44 -0300

    refactor(inputPatchSelect.py):Show sidebar

--- Pix/Modules/Prompts/inputPatchSelect.py
+++ Pix/Modules/Prompts/inputPatchSelect.py
@@ -5,30 +5,34 @@ def patchSelect(lines=[], seleccionableLinesIncludes=[], fileName="", errorMessa
 
     print("\n On " + fileName + "\n")
 
-    _, terminalWidth = popen("stty size", "r").read().split()
+    terminalHeight, terminalWidth = popen("stty size", "r").read().split()
     terminalWidth = int(terminalWidth) - 1
+    selectionAreaHeight = int(terminalHeight) - 4
 
     reset = "\x1b[0m"
     bold = "\x1b[1m"
     dim = "\x1b[2m"
-    colors = {"+": "\x1b[32m", "-": "\x1b[31m"}
+    colors = {"+": f"{bold}\x1b[32m", "-": f"{bold}\x1b[31m"}
+    border = f"\x1b[36m|{reset}"
 
     getch = getGetch()
-    inputConsole = ConsoleControl(15)
+    inputConsole = ConsoleControl(selectionAreaHeight)
     textZoneArea = range(0, len(lines))
 
     offset = 0
     patchesSelected = []
 
     def updateConsole():
-        for lineNumber in range(0, 15):
+        for lineNumber in range(0, selectionAreaHeight):
             index = lineNumber + offset
             lineText = lines[index].strip() if index in textZoneArea else ""
             lineTextLimited = lineText[0:terminalWidth]
             if len(lineText) and lineText[0] in colors:
                 lineTextLimited = colors[lineText[0]] + lineTextLimited + reset
+            else:
+                lineTextLimited = dim + lineTextLimited + reset
 
-            inputConsole.setConsoleLine(lineNumber, 1, lineTextLimited)
+            inputConsole.setConsoleLine(lineNumber, 1, f"{border}   {lineTextLimited}")
 
         inputConsole.refresh()
 
@@ -43,9 +47,11 @@ def patchSelect(lines=[], seleccionableLinesIncludes=[], fileName="", errorMessa
         state = getMovement(char)
 
         if state == "DOWN":
-            offset = offset + 1
+            newOffset = offset + 1
+            offset = newOffset if newOffset in textZoneArea and len(lines) > selectionAreaHeight else offset
         elif state == "UP":
-            offset = offset - 1
+            newOffset = offset - 1
+            offset = newOffset if newOffset in textZoneArea else offset
         elif state == "RIGHT":
             lineText = lines[offset + 4]
             saveLineText(True, lineText)
@@ -55,12 +61,12 @@ def patchSelect(lines=[], seleccionableLinesIncludes=[], fileName="", errorMessa
         elif state == "FINISH":
             break
         elif state == "BREAK_CHAR":
-            inputConsole.deleteLastLines(9)
+            inputConsole.deleteLastLines(selectionAreaHeight + 4)
             inputConsole.finish()
             print(errorMessage)
             exit()
 
-    inputConsole.deleteLastLines(9)
+    inputConsole.deleteLastLines(selectionAreaHeight)
     inputConsole.finish()
 
     return patchesSelected
