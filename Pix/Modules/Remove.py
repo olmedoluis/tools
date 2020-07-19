c0f248cdb29ca8d6057ef631997b887853224283
Luis Olmedo <olmedoluis012@gmail.com>
2020-07-19 16:03:48 -0300

    refactor(Remove.py):Implement class message control

--- Pix/Modules/Remove.py
+++ Pix/Modules/Remove.py
@@ -1,10 +1,11 @@
-from .Helpers import run, removeColors
+from .Helpers import run, removeColors, MessageControl
 from .Status import getStatus, searchInStatus
 
 
 def remove(filePaths=[], shouldVerify=True):
     from .Prompts import multiSelect
 
+    m = MessageControl()
     status = getStatus()
 
     specificFiles = filePaths if filePaths else []
@@ -13,45 +14,44 @@ def remove(filePaths=[], shouldVerify=True):
 
     if len(specificFiles) == 1 or not shouldVerify:
         run(["git", "reset", "HEAD"] + specificFiles)
-        return print(messages["remove-success"])
+        return m.log("remove-success")
 
     options = status["added"] if "added" in status else []
     options = specificFiles if len(specificFiles) != 0 else options
 
     if len(options) == 0:
-        return print(messages["remove-nofiles-error"])
+        return m.log("remove-nofiles-error")
 
     print()
     answers = multiSelect(
-        title=messages["remove-removing-title"],
-        finalTitle=messages["file-selection-finaltitle"],
+        title=m.getMessage("remove-removing-title"),
+        finalTitle=m.getMessage("file-selection-finaltitle"),
         options=options,
         selectedColor="\x1b[31m",
     )
 
     if answers == "UNKNOWN_ERROR":
-        return print(messages["unknown-error"])
+        return m.log("unknown-error")
     if len(answers) == 0:
-        return print(messages["remove-nofileschoosen-error"])
+        return m.log("remove-nofileschoosen-error")
 
     choices = []
     for answer in answers:
         choices.append(removeColors(answer))
 
     run(["git", "reset", "HEAD"] + choices)
-    print(messages["remove-success"])
+    m.log("remove-success")
 
 
 def removeAll(fileSearch):
+    m = MessageControl()
     status = getStatus()
 
     if len(fileSearch) > 0:
         matches = searchInStatus(fileSearch, status, includedFiles=["added"])
 
         return (
-            print(messages["error-nomatchfile"])
-            if len(matches) == 0
-            else remove(matches, False)
+            m.log("error-nomatchfile") if len(matches) == 0 else remove(matches, False)
         )
 
     hasFilesToRemove = False
@@ -64,19 +64,12 @@ def removeAll(fileSearch):
 
     if hasFilesToRemove:
         run(["git", "reset", "HEAD", "."])
-        print(messages["remove-all-success"])
+        m.log("remove-all-success")
     else:
-        print(messages["remove-all-nofiles"])
-
-
-def setUp(outsideMessages):
-    global messages
-    messages = outsideMessages
+        m.log("remove-all-nofiles")
 
 
 def Router(router, subroute):
-    setUp(router.messages)
-
     if subroute == "REMOVE_ALL":
         removeAll(router.leftKeys[1:])
     if subroute == "DEFAULT":
