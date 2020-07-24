edd905c682ac544f60836f34a4aee5b770b3044e
Luis Olmedo <olmedoluis012@gmail.com>
2020-07-24 00:21:20 -0300

    refactor(Theme.py):Remove unnessary themes

--- Pix/Data/Theme.py
+++ Pix/Data/Theme.py
@@ -1,16 +1,5 @@
 RESET = "\x1b[0m" + "\u001b[0m"
-BOLD = "\u001b[1m"
-DIM = "\x1b[2m"
-
-RED_LOW = "\x1b[31m"
-YELLOW_LOW = "\x1b[33m"
-
-RED_HIGH = "\x1b[91m"
-GREEN_HIGH = "\x1b[92m"
-BLUE_HIGH = "\x1b[94m"
-MAGENTA_HIGH = "\x1b[95m"
-WHITE_HIGH = "\x1b[97m"
-CODE = u"\u001b[38;5;"
+CODE = "\u001b[38;5;"
 
 THEME = {
     "th_normal": f"{CODE}15;1m",
@@ -23,7 +12,7 @@ THEME = {
     "th_renamed": f"{CODE}203;1m",
     "th_conflicted": f"{CODE}209;1m",
     "th_error": f"{CODE}9;1m",
-    "th_dim": u"\u001b[2m"
+    "th_dim": "\u001b[2m",
 }
 
 
