97e433634385f7dddfd1aba0d8918009883429d9
Luis Olmedo <olmedoluis012@gmail.com>
2020-07-25 15:10:28 -0300

    refactor(Theme.py):Rename theme

--- Pix/Modules/Prompts/Theme.py
+++ Pix/Modules/Prompts/Theme.py
@@ -6,7 +6,7 @@ INPUT_THEME = {
     "modification": f"{CODE}221;1m",
     "deletation": f"{CODE}203;1m",
     "slight": "\u001b[2m",
-    "indexAct": f"{CODE}69;1m",
+    "selection": f"{CODE}69;1m",
     "indexSel": f"{CODE}48;1m",
     "index": f"{CODE}15;1m",
     "fileAct": f"{CODE}171;1m",
