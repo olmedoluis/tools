1dbca6db917bc2027afcddeaed5010a2e5af8fa7
Luis Olmedo <olmedoluis012@gmail.com>
2020-07-26 15:06:01 -0300

    refactor(Modules/__init__.py):Set up pix store

--- Pix/Modules/__init__.py
+++ Pix/Modules/__init__.py
@@ -5,3 +5,14 @@ from .Commit import Router as CommitRouter
 from .Remove import Router as RemoveRouter
 from .Stash import Router as StashRouter
 from .Patch import Router as PatchRouter
+
+PIX_STORE = {
+    "Status": StatusRouter,
+    "Add": AddRouter,
+    "Remove": RemoveRouter,
+    "Commit": CommitRouter,
+    "Branch": BranchRouter,
+    "Stash": StashRouter,
+    "Patch": PatchRouter,
+}
+
