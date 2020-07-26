1bead8dafca58fcb8bcba4eb775b65facec0c089
Luis Olmedo <olmedoluis012@gmail.com>
2020-07-26 01:57:12 -0300

    style(Theme.py):Sort themes alphabetic

--- Configuration/Theme.py
+++ Configuration/Theme.py
@@ -1,40 +1,40 @@
-RESET = "\u001b[0m"
-CODE = "\u001b[38;5;"
+ANSI = "\u001b["
+CODE = f"{ANSI}38;5;"
 
 THEME = {
-    "th_normal": f"{CODE}15;1m",
-    "th_success": f"{CODE}47;1m",
-    "th_keyword": f"{CODE}171;1m",
     "th_added": f"{CODE}48;1m",
-    "th_modified": f"{CODE}221;1m",
-    "th_deleted": f"{CODE}203;1m",
-    "th_untracked": f"{CODE}69;1m",
-    "th_renamed": f"{CODE}203;1m",
     "th_conflicted": f"{CODE}209;1m",
+    "th_deleted": f"{CODE}203;1m",
+    "th_dim": f"{ANSI}2m",
     "th_error": f"{CODE}9;1m",
-    "th_dim": "\u001b[2m",
-    "th_reset": RESET,
+    "th_keyword": f"{CODE}171;1m",
+    "th_modified": f"{CODE}221;1m",
+    "th_normal": f"{CODE}15;1m",
+    "th_renamed": f"{CODE}203;1m",
+    "th_reset": f"{ANSI}0m",
+    "th_success": f"{CODE}47;1m",
+    "th_untracked": f"{CODE}69;1m",
 }
 
 ICONS = {"ic_modified": "☢", "ic_error": "✖", "ic_selection": "❤", "ic_normal": "•"}
 
 EMPTY = {}
 INPUT_THEME = {
-    "COMMIT_CREATION_TYPE": {"selection": f"{CODE}221;1m"},
-    "COMMIT_CREATION_SCOPE": EMPTY,
-    "COMMIT_CREATION_ABOUT": EMPTY,
-    "COMMIT_CREATION_CONFIRM": EMPTY,
     "ADD_SELECTION": {"selection": f"{CODE}48;1m"},
-    "REMOVE_SELECTION": {"selection": f"{CODE}9;1m"},
-    "BRANCH_SELECTION": {"selection": f"{CODE}171;1m"},
-    "BRANCH_CREATION_TYPE": {"selection": f"{CODE}221;1m"},
-    "BRANCH_CREATION_ID": EMPTY,
     "BRANCH_CREATION_ABOUT": EMPTY,
     "BRANCH_CREATION_CONFIRM": EMPTY,
+    "BRANCH_CREATION_ID": EMPTY,
     "BRANCH_CREATION_SWITCH": EMPTY,
-    "STASH_SELECTION": EMPTY,
-    "STASH_CREATION_NAME": EMPTY,
+    "BRANCH_CREATION_TYPE": {"selection": f"{CODE}221;1m"},
+    "BRANCH_SELECTION": {"selection": f"{CODE}171;1m"},
+    "COMMIT_CREATION_ABOUT": EMPTY,
+    "COMMIT_CREATION_CONFIRM": EMPTY,
+    "COMMIT_CREATION_SCOPE": EMPTY,
+    "COMMIT_CREATION_TYPE": {"selection": f"{CODE}221;1m"},
     "PATCH_SELECTION": EMPTY,
+    "REMOVE_SELECTION": {"selection": f"{CODE}9;1m"},
+    "STASH_CREATION_NAME": EMPTY,
+    "STASH_SELECTION": EMPTY,
 }
 
 INPUT_ICONS = {
