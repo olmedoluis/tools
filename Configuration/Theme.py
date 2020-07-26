219e18204191b43c24c6598e551cfe82a4a1d331
Luis Olmedo <olmedoluis012@gmail.com>
2020-07-26 01:57:56 -0300

    refactor(Theme.py):Remove unnessary comment

--- Configuration/Theme.py
+++ Configuration/Theme.py
@@ -43,11 +43,3 @@ INPUT_ICONS = {
     "selection": ICONS["ic_selection"],
     "normal": ICONS["ic_normal"],
 }
-
-
-# import sys
-#     for i in range(0, 16):
-#         for j in range(0, 16):
-#             code = str(i * 16 + j)
-#             sys.stdout.write(u"\u001b[38;5;" + code + "m " + code.ljust(4))
-#         print (u"\u001b[0m")
