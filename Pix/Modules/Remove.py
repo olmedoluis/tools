1c3351db7a4a36fe5b97d41646ceaa4bb4ccdc07
Luis Olmedo <olmedoluis012@gmail.com>
2020-09-19 18:11:28 -0300

    refactor(Remove.py):Add posibility to remove indivisually

--- Pix/Modules/Remove.py
+++ Pix/Modules/Remove.py
@@ -1,10 +1,10 @@
-def remove(file_paths=[], should_verify=True):
+def remove(file_paths=[], use_availables=False, messages=""):
     from .Prompts import multi_select
     from .Helpers import run, removeColors, MessageControl
     from .Status import get_status, search_in_status, get_status_paths
     from Configuration.Theme import INPUT_THEME, INPUT_ICONS
 
-    m = MessageControl()
+    m = MessageControl() if messages == "" else messages
     status = get_status()
 
     file_paths = (
@@ -15,7 +15,7 @@ def remove(file_paths=[], should_verify=True):
 
     if len(file_paths) == 0:
         return m.log("error-remove-files_not_found")
-    elif not should_verify:
+    elif use_availables:
         run(["git", "reset"] + file_paths)
         return m.log("remove-all-success")
 
@@ -40,8 +40,22 @@ def remove(file_paths=[], should_verify=True):
     m.log("remove-success")
 
 
+def remove_individually(file_paths):
+    from .Helpers import MessageControl
+
+    m = MessageControl()
+
+    if len(file_paths):
+        for file_path in file_paths:
+            remove(file_paths=[file_path], messages=m)
+
+        return m.log("remove-success")
+
+    remove(messages=m)
+
+
 def router(argument_manager, sub_route):
     if sub_route == "REMOVE_ALL":
-        remove(file_paths=[], should_verify=False)
+        remove(file_paths=[], use_availables=True)
     if sub_route == "DEFAULT":
-        remove(argument_manager.left_keys)
+        remove_individually(argument_manager.left_keys)
