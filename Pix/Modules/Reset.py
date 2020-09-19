2d769eac086ab1cc89f0a7546a9da95640d2355d
Luis Olmedo <olmedoluis012@gmail.com>
2020-09-18 22:13:50 -0300

    refactor(Reset.py):Set up reset functionality

--- Pix/Modules/Reset.py
+++ Pix/Modules/Reset.py
@@ -1,5 +1,83 @@
-def reset(files):
-    print("reseting...", files)
+def parseStatus(status):
+    resetCommands = []
+
+    for statusId in status:
+        for change in status[statusId]:
+            if statusId == "untracked":
+                resetCommands.append(["rm", change])
+            else:
+                resetCommands.append(["git", "checkout", change])
+
+    return resetCommands
+
+
+def reset(filePaths=[], shouldVerify=True, messages=""):
+    from .Prompts import multiSelect
+    from .Helpers import runAll, removeColors, MessageControl
+    from .Status import getStatus, searchInStatus
+    from Configuration.Theme import INPUT_THEME, INPUT_ICONS
+
+    m = MessageControl() if messages == "" else messages
+    status = getStatus()
+
+    filesForParsing = []
+    specificFiles = filePaths if filePaths else []
+
+    if len(specificFiles) == 0:
+        for statusId in status:
+            statusContent = status[statusId]
+            if statusId == "branch" or statusId == "added":
+                continue
+
+            specificFiles = specificFiles + statusContent
+
+    if len(filePaths) != 0 and shouldVerify:
+        filesForParsing = searchInStatus(
+            filePaths,
+            status,
+            excludedFiles=["branch", "added"],
+            getOriginalStructure=True,
+        )
+        specificFiles = searchInStatus(
+            filePaths, status, excludedFiles=["branch", "added"]
+        )
+
+    if not shouldVerify:
+        commands = parseStatus(filesForParsing)
+
+        runAll(commands)
+        return m.log("add-success")
+
+    if len(specificFiles) == 0:
+        return m.log("error-add-files_not_found")
+
+    print()
+    answers = multiSelect(
+        title=m.getMessage("add-title"),
+        finalTitle=m.getMessage("file-selection-finaltitle"),
+        errorMessage=m.getMessage("error-files_selected_not_found"),
+        options=specificFiles,
+        colors=INPUT_THEME["ADD_SELECTION"],
+        icons=INPUT_ICONS,
+    )
+
+    if len(answers) == 0:
+        return m.log("error-files_selected_not_found")
+
+    choices = []
+    for answer in answers:
+        choices.append(removeColors(answer))
+
+    filesForParsing = searchInStatus(
+        choices,
+        status,
+        excludedFiles=["branch", "added"],
+        getOriginalStructure=True,
+    )
+
+    runAll(parseStatus(filesForParsing))
+    # m.log("add-success")
+
 
 def Router(router, subroute):
     if subroute == "DEFAULT":
