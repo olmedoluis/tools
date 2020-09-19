94e0f1b6968e9e7adf14b4730074e3d2372efbd3
Luis Olmedo <olmedoluis012@gmail.com>
2020-09-19 12:10:11 -0300

    fix(__init__.py):Update renamed router import

--- Pix/Modules/__init__.py
+++ Pix/Modules/__init__.py
@@ -1,5 +1,5 @@
 from .Status import Router as StatusRouter
-from .Add import Router as AddRouter
+from .Add import router as AddRouter
 from .Branch import Router as BranchRouter
 from .Commit import Router as CommitRouter
 from .Remove import Router as RemoveRouter
