5d2a51da60d403aca1676cd78446a6d3eec85260
Luis Olmedo <olmedoluis012@gmail.com>
2020-09-19 15:13:01 -0300

    refactor(__init__.py):Properly apply pep8 naming convention

--- Pix/Modules/Prompts/__init__.py
+++ Pix/Modules/Prompts/__init__.py
@@ -5,7 +5,7 @@ from .InputMultiSelect import multi_select
 from .inputPatchSelect import patch_select
 
 
-def many(selectedInputsData):
+def many(selected_inputs_data):
     STORE = {
         "text": text,
         "select": select,
@@ -15,10 +15,10 @@ def many(selectedInputsData):
     }
     responses = []
 
-    for selectedInputData in selectedInputsData:
-        selectedInput = STORE[selectedInputData["type"]]
-        selectedInputData.pop("type")
-        response = selectedInput(**selectedInputData)
+    for selected_input_data in selected_inputs_data:
+        selected_input = STORE[selected_input_data["type"]]
+        selected_input_data.pop("type")
+        response = selected_input(**selected_input_data)
 
         if response == "":
             break
