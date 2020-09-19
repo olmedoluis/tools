4ae12752b74edd7d397f56b5d6ed3dc4a11c2b1e
Luis Olmedo <olmedoluis012@gmail.com>
2020-09-19 12:50:19 -0300

    style(Branch.py):Use pep8 naming convention

--- Pix/Modules/Branch.py
+++ Pix/Modules/Branch.py
@@ -1,4 +1,4 @@
-def getHasChanges(change=""):
+def get_has_changes(change=""):
     from .Status import getStatus
 
     status = getStatus()
@@ -6,48 +6,49 @@ def getHasChanges(change=""):
     return len(status.keys()) > 1 if change == "" else change in status
 
 
-def branchSelection(branchSearch):
+def branchSelection(branch_search):
     from .Prompts import select
     from .Helpers import run, MessageControl
 
     m = MessageControl()
-    hasChanges = getHasChanges()
+    has_changes = get_has_changes()
 
-    if hasChanges:
+    if has_changes:
         return m.log("error-haschanges")
 
-    branchesOutput = run(["git", "branch"])
-    branchesSpaced = branchesOutput.rstrip().split("\n")
+    branches_raw = run(["git", "branch"])
+    branches_spaced = branches_raw.rstrip().split("\n")
 
-    if branchesSpaced[0] == "":
+    if branches_spaced[0] == "":
         return m.log("error-branch-branches_not_found")
 
-    if branchSearch != "":
-        branchMatch = []
-        for branch in branchesSpaced:
-            if branch.find(branchSearch) != -1:
-                branchMatch.append(branch)
+    if branch_search != "":
+        branch_match = []
+        for branch in branches_spaced:
+            if branch.find(branch_search) != -1:
+                branch_match.append(branch)
 
-        if len(branchMatch) == 0:
-            return m.log("error-branch-match_not_found", {"pm_branch": branchSearch})
+        if len(branch_match) == 0:
+            return m.log("error-branch-match_not_found", {"pm_branch": branch_search})
 
-        branchesSpaced = branchMatch
+        branches_spaced = branch_match
 
     branches = []
-    actualBranch = ""
-    for branch in branchesSpaced:
+    actual_branch = ""
+    for branch in branches_spaced:
         if branch[0] == "*":
             branch = branch[1:]
-            actualBranch = branch.lstrip()
+            actual_branch = branch.lstrip()
+
         branches.append(branch.lstrip())
 
-    branchSelected = branches[0]
+    branch_selected = branches[0]
 
     if len(branches) > 1:
         from Configuration.Theme import INPUT_THEME, INPUT_ICONS
 
         print()
-        branchSelected = select(
+        branch_selected = select(
             title=m.getMessage("branch-selection-title"),
             options=branches,
             errorMessage=m.getMessage("operation-cancel"),
@@ -55,14 +56,14 @@ def branchSelection(branchSearch):
             icons=INPUT_ICONS,
         )
 
-        if branchSelected == "":
+        if branch_selected == "":
             return m.log("error-empty")
 
-    if branchSelected != actualBranch:
-        run(["git", "checkout", branchSelected])
-        m.log("branch-success", {"pm_branch": branchSelected})
+    if branch_selected != actual_branch:
+        run(["git", "checkout", branch_selected])
+        m.log("branch-success", {"pm_branch": branch_selected})
     else:
-        m.log("branch-selection-same_branch", {"pm_branch": branchSelected})
+        m.log("branch-selection-same_branch", {"pm_branch": branch_selected})
 
 
 def branchCreation():
@@ -71,13 +72,13 @@ def branchCreation():
     from Configuration.Theme import INPUT_THEME, INPUT_ICONS
 
     m = MessageControl()
-    hasChanges = getHasChanges()
+    has_changes = get_has_changes()
 
-    if hasChanges:
+    if has_changes:
         return m.log("error-haschanges")
 
     options = ["feature", "refactor", "bugfix", "style"]
-    scapeError = m.getMessage("operation-cancel")
+    scape_error = m.getMessage("operation-cancel")
 
     print()
     answers = many(
@@ -86,7 +87,7 @@ def branchCreation():
                 "type": "Select",
                 "title": m.getMessage("branch-creation-type_title"),
                 "options": options,
-                "errorMessage": scapeError,
+                "errorMessage": scape_error,
                 "colors": INPUT_THEME["BRANCH_CREATION_TYPE"],
                 "icons": INPUT_ICONS,
             },
@@ -94,13 +95,13 @@ def branchCreation():
                 "type": "Text",
                 "title": m.getMessage("branch-creation-id_title"),
                 "placeHolder": "",
-                "errorMessage": scapeError,
+                "errorMessage": scape_error,
                 "colors": INPUT_THEME["BRANCH_CREATION_ID"],
             },
             {
                 "type": "Text",
                 "title": m.getMessage("branch-creation-about_title"),
-                "errorMessage": scapeError,
+                "errorMessage": scape_error,
                 "colors": INPUT_THEME["BRANCH_CREATION_ABOUT"],
             },
         ]
@@ -109,31 +110,31 @@ def branchCreation():
     if len(answers) != 3:
         return m.log("error-empty")
 
-    kind, ticketId, about = answers
-    ticketId = ticketId.upper().replace(" ", "-")
+    kind, ticket_id, about = answers
+    ticket_id = ticket_id.upper().replace(" ", "-")
     about = about.lower().replace(" ", "-")
 
-    branch = f"{kind}/{ticketId}-{about}"
+    branch = f"{kind}/{ticket_id}-{about}"
 
     m.log("preview", {"pm_preview": branch})
 
-    isSure = confirm(
+    should_create_branch = confirm(
         title=m.getMessage("confirmation"),
         colors=INPUT_THEME["BRANCH_CREATION_CONFIRM"],
     )
 
-    if isSure:
+    if should_create_branch:
         run(["git", "branch", branch])
         m.log("branch-success", {"pm_branch": branch})
     else:
-        return m.log("commit-cancel")
+        return m.log("operation-cancel")
 
-    shouldSwitch = confirm(
+    should_switch = confirm(
         title=m.getMessage("branch-creation-shouldswitch"),
         colors=INPUT_THEME["BRANCH_CREATION_SWITCH"],
     )
 
-    if shouldSwitch:
+    if should_switch:
         run(["git", "checkout", branch])
         m.log("branch-creation-switch_success", {"pm_branch": branch})
     else:
