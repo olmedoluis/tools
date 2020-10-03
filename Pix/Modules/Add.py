6a126d960300aefbfe5d79c49fb00bedae8b9325
Luis Olmedo <olmedoluis012@gmail.com>
2020-10-03 15:35:41 -0300

    refactor(Add.py):Properly add files

--- Pix/Modules/Add.py
+++ Pix/Modules/Add.py
@@ -7,7 +7,6 @@ def add(file_paths=[], use_availables=False, messages="", show_logs=True):
     m = MessageControl() if messages == "" else messages
     status = get_status(ignoreColors=True)
 
-    is_individual_path = len(file_paths) == 1
     file_paths = (
         search_in_status(file_paths, status, excluded_files=["branch", "added"])
         if len(file_paths)
@@ -19,31 +18,28 @@ def add(file_paths=[], use_availables=False, messages="", show_logs=True):
         exit()
 
     elif use_availables:
-        run(["git", "add"] + file_paths)
-        m.log("add-success")
-        m.logMany(message_id="add-file", param_name="pm_file", contents=file_paths)
-        return
+        pass
 
-    elif is_individual_path and len(file_paths) == 1:
-        return run(["git", "add"] + file_paths)
+    elif len(file_paths) != 1:
+        print()
+        answers = multi_select(
+            title=m.get_message("add-title"),
+            final_title=m.get_message("file-selection-finaltitle"),
+            error_message=m.get_message("error-files_selected_not_found"),
+            options=file_paths,
+            colors=INPUT_THEME["ADD_SELECTION"],
+            icons=INPUT_ICONS,
+        )
 
-    print()
-    answers = multi_select(
-        title=m.get_message("add-title"),
-        final_title=m.get_message("file-selection-finaltitle"),
-        error_message=m.get_message("error-files_selected_not_found"),
-        options=file_paths,
-        colors=INPUT_THEME["ADD_SELECTION"],
-        icons=INPUT_ICONS,
-    )
+        if len(answers) == 0:
+            return m.log("error-files_selected_not_found")
 
-    if len(answers) == 0:
-        return m.log("error-files_selected_not_found")
+        file_paths = answers
 
-    run(["git", "add"] + answers)
+    run(["git", "add"] + file_paths)
     if show_logs:
         m.log("add-success")
-        m.logMany(message_id="add-file", param_name="pm_file", contents=answers)
+        m.logMany(message_id="add-file", param_name="pm_file", contents=file_paths)
 
 
 def add_individually(file_paths):
