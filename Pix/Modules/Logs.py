3fd242ea26cc884122157ddba3d9fd42eed9341d
Luis Olmedo <olmedoluis012@gmail.com>
2020-10-03 18:21:23 -0300

    refactor(Logs.py):Get array of logs

--- Pix/Modules/Logs.py
+++ Pix/Modules/Logs.py
@@ -1,5 +1,46 @@
+class Log:
+    def __init__(self, hash, author, time, commit):
+        self.hash = hash
+        self.author = author
+        self.time = time
+        self.commit = commit
+
+
+def get_logs(logs_data):
+    logs = []
+
+    for log_line in logs_data:
+        log_array = log_line.split("/*/")
+
+        logs.append(
+            Log(
+                hash=log_array[0],
+                author=log_array[1],
+                time=log_array[2],
+                commit=log_array[3],
+            )
+        )
+
+    return logs
+
+
 def log():
-    print("logging commits..")
+    from .Helpers import run
+    from .Branch import get_branch_creator
+
+    branch_creator = get_branch_creator()
+    logs_raw = run(
+        [
+            "git",
+            "log",
+            "--first-parent",
+            "--oneline",
+            f"{branch_creator}..",
+            '--pretty=format:"%h/*/%an/*/%ar/*/%s"',
+        ]
+    )
+
+    logs = get_logs(logs_raw.split("\n"))
 
 
 def router(argument_parser, sub_route):
