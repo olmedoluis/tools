0b0f1a746ffab57ff53bf27ad5d0c74ace792633
Luis Olmedo <olmedoluis012@gmail.com>
2020-10-03 15:42:25 -0300

    refactor(Reset.py):Properly reset files

--- Pix/Modules/Reset.py
+++ Pix/Modules/Reset.py
@@ -40,7 +40,6 @@ def reset(file_paths=[], use_availables=False, messages="", show_logs=True):
     m = MessageControl() if messages == "" else messages
     status = get_status(ignoreColors=True)
 
-    is_individual_path = len(file_paths) == 1
     file_paths = (
         search_in_status(file_paths, status, excluded_files=["branch", "added"])
         if len(file_paths)
@@ -50,32 +49,30 @@ def reset(file_paths=[], use_availables=False, messages="", show_logs=True):
     if len(file_paths) == 0:
         m.log("error-reset-files_not_found")
         exit()
+
     elif use_availables:
-        executeReset(file_paths, status)
-        m.log("reset-success")
-        m.logMany(message_id="reset-file", param_name="pm_file", contents=file_paths)
-        return
-    elif is_individual_path and len(file_paths) == 1:
-        return executeReset(file_paths, status)
+        pass
 
-    print()
-    answers = multi_select(
-        title=m.get_message("reset-title"),
-        final_title=m.get_message("file-selection-finaltitle"),
-        error_message=m.get_message("error-files_selected_not_found"),
-        options=file_paths,
-        colors=INPUT_THEME["RESET_SELECTION"],
-        icons=INPUT_ICONS,
-    )
+    elif len(file_paths) != 1:
+        print()
+        answers = multi_select(
+            title=m.get_message("reset-title"),
+            final_title=m.get_message("file-selection-finaltitle"),
+            error_message=m.get_message("error-files_selected_not_found"),
+            options=file_paths,
+            colors=INPUT_THEME["RESET_SELECTION"],
+            icons=INPUT_ICONS,
+        )
 
-    if len(answers) == 0:
-        return m.log("error-files_selected_not_found")
+        if len(answers) == 0:
+            return m.log("error-files_selected_not_found")
 
-    executeReset(answers, status)
+        file_paths = answers
 
+    executeReset(file_paths, status)
     if show_logs:
         m.log("reset-success")
-        m.logMany(message_id="reset-file", param_name="pm_file", contents=answers)
+        m.logMany(message_id="reset-file", param_name="pm_file", contents=file_paths)
 
 
 def reset_individually(file_paths):
