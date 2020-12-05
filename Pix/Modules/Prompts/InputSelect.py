b3a5708c723a124fa3bcbbdc64324d34569e7236
Luis Olmedo <olmedoluis012@gmail.com>
2020-12-05 00:33:21 -0300

    feat(InputSelect.py):Add edition mode

--- Pix/Modules/Prompts/InputSelect.py
+++ Pix/Modules/Prompts/InputSelect.py
@@ -11,11 +11,11 @@ def select(
 
     getch = getGetch()
     input_console = ConsoleControl(5)
-    select_control = _SelectControl(colors, icons, options)
-
-    input_console.setConsoleLine(0, 1, title)
+    select_control = _SelectControl(colors, icons, options, title)
 
     while True:
+        input_console.setConsoleLine(0, 1, select_control.get_display_title())
+
         display_text = select_control.get_display_option(-1)
         input_console.setConsoleLine(2, 6, display_text)
 
@@ -29,7 +29,13 @@ def select(
         char = getch()
         state = get_parsed_char(char)
 
-        if state == "S":
+        if select_control.is_filter_enabled and len(state) == 1:
+            select_control.set_filtering(select_control.filtering + char)
+        elif select_control.is_filter_enabled and state == "BACKSTAB":
+            select_control.set_filtering(select_control.filtering[:-1])
+        elif state == "F":
+            select_control.toggleFilteringMode()
+        elif state == "S":
             select_control.append_to_index(1)
         elif state == "W":
             select_control.append_to_index(-1)
@@ -53,7 +59,7 @@ def select(
 
 
 class _SelectControl:
-    def __init__(self, colors, icons, options):
+    def __init__(self, colors, icons, options, title):
         from .Theme import INPUT_THEME, INPUT_ICONS
 
         self._SELECTION_ICON = ({**INPUT_ICONS, **icons})["selection"]
@@ -63,6 +69,11 @@ class _SelectControl:
         self._options = options
         self._options_size = len(options)
         self._option_index = 0
+
+        self._options_filtered = options
+        self.filtering = ""
+        self.is_filter_enabled = False
+        self.title = title
 
     def get_display_option(self, offset=0):
         font_color = self._COLORS["slight"] if offset else self._COLORS["selection"]
@@ -76,3 +87,14 @@ class _SelectControl:
 
     def append_to_index(self, number):
         self._option_index = self._option_index + number
+
+    def toggleFilteringMode(self):
+        self.is_filter_enabled = not self.is_filter_enabled
+
+    def get_display_title(self):
+        filter_value = self.filtering if self.is_filter_enabled else ""
+
+        return f"{self.title} {filter_value}"
+
+    def set_filtering(self, new_value):
+        self.filtering = new_value
