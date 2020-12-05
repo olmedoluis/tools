1e42a5a48b7bc9d697e98e03da0b020babd6d45c
Luis Olmedo <olmedoluis012@gmail.com>
2020-12-05 13:28:02 -0300

    feat(CharactersInterpreter.py):Add filter control class

--- Pix/Modules/Prompts/CharactersInterpreter.py
+++ Pix/Modules/Prompts/CharactersInterpreter.py
@@ -16,3 +16,39 @@ def get_parsed_char(char):
 
     else:
         return "UNKNOWN"
+
+
+class FiltersControl:
+    def __init__(
+        self,
+        colors,
+        place_holder='Press "F" to filter',
+        active_place_holder="Write something here",
+    ):
+        self._COLORS = colors
+        self._RESET = colors["reset"]
+        self.filtering = ""
+        self.is_filter_enabled = False
+        self.place_holder = place_holder
+        self.active_place_holder = active_place_holder
+
+    def get_filter_value(self):
+        slight = self._COLORS["slight"]
+        color = self._COLORS["modification"] if self.is_filter_enabled else slight
+        place_holder = (
+            f"{slight}{self.active_place_holder}"
+            if self.is_filter_enabled
+            else self.place_holder
+        )
+
+        return (
+            f"{color}{self.filtering}{self._RESET}"
+            if self.filtering != ""
+            else f"{color}{place_holder}{self._RESET}"
+        )
+
+    def toggle_filtering_mode(self):
+        self.is_filter_enabled = not self.is_filter_enabled
+
+    def set_filter(self, value):
+        self.filtering = value
