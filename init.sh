3075c295b8521f2d149cc83c564b28b2171fb9a7
olmedoluis <olmedoluis012@gmail.com>
2024-06-05 23:03:37 -0300

    feat: Expect for folders

--- init.sh
+++ init.sh
@@ -16,11 +16,24 @@ function tools-init () {
         echo -e "# [TOOLS VARIABLES]" >> "$BASH_PROFILE"
         echo -e "TOOLS_PATH=\"$TOOLS_PATH\"" >> "$BASH_PROFILE"
         echo -e "source \"$TOOLS_PATH/main.sh\"" >> "$BASH_PROFILE"
+        echo -e "# [TOOLS VARIABLES]" >> "$BASH_PROFILE"
         
         echo -e "${TAB}${ISTAR}${GREEN}${IOK}Tools variables added.${END_COLOR}"
     else
         echo -e "${TAB}${ISTAR}${YELLOW}${IWARN}Tools variables already exist.${END_COLOR}"I
     fi
+    
+    source "$BASH_PROFILE"
+    
+    
+    if ! [[ -d "$TOOLS_PATH/temp" ]]; then
+        mkdir "$TOOLS_PATH/temp"
+    fi
+    
+    local built_in_plugins=$( ls -l "$TOOLS_PATH/plugins" | grep "^d" | awk '{print $NF}')
+    for plugin_name in "${built_in_plugins[@]}"; do
+        tools add-plugin "$TOOLS_PATH/plugins/$plugin_name"
+    done
     
     source "$BASH_PROFILE"
     
