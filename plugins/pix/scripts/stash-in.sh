d886ae2b3d282de21556ed2a6e0e52a874d82220
olmedoluis <olmedoluis012@gmail.com>
2024-06-12 02:54:18 -0300

    feat: Partial fix

--- plugins/pix/scripts/stash-in.sh
+++ plugins/pix/scripts/stash-in.sh
@@ -8,12 +8,12 @@ function stash-in() {
         return 1
     fi
     
-    staged_files_stringified=$(git diff --cached --name-only)
+    staged_files=($(git diff --cached --name-only))
     staged_files_count=$(git diff --cached --name-only | wc -l)
     
     if [ "$staged_files_count" -gt 0 ]; then
         echo -e "${IINFO}Saving staged file(s) in stash storage."
-        git stash push --quiet $staged_files_stringified > /dev/null 2>&1
+        git stash push --quiet -- ${staged_files[@]} > /dev/null 2>&1
         
         echo -e "${TAB}${IDOT}${GREEN}${IOK}$staged_files_count file(s) saved in stash storage.${END_COLOR}"
     else
