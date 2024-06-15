8a3bf2c2dcf0eb15e2760f03b51db00db4d20bd6
olmedoluis <olmedoluis012@gmail.com>
2024-06-15 00:13:05 -0300

    feat: Expect for inital pattern in commits

--- plugins/pix/scripts/commit.sh
+++ plugins/pix/scripts/commit.sh
@@ -11,18 +11,24 @@ function commit() {
     fi
     
     local CONFIG_FILE=$1
+    local pattern="$2"
     
     if [ ! -f "$CONFIG_FILE" ]; then
         echo -e "${RED}${IERR}Config JSON file not found.${END_COLOR}"
         return 1
     fi
     
-    local pattern=($(read_json_array $CONFIG_FILE commit_rules.pattern))
-    pattern="${pattern[@]}"
-    
-    echo -e "${IINFO}Creating form."
-    create_form "$CONFIG_FILE" "commit_rules.form" "$pattern"
-    echo -e "${TAB}⋆ ${GREEN}${IOK}Commit form completed.${END_COLOR}"
+    if [[ -z $pattern ]]; then
+        echo -e "${IINFO}Creating form."
+        pattern=$(read_json_array "$CONFIG_FILE" "commit_rules.pattern")
+        pattern="${pattern[@]}"
+        
+        create_form "$CONFIG_FILE" "branch_rules.form" "$pattern"
+        echo -e "${TAB}${ISTAR}${GREEN}${IOK}Commit form completed.${END_COLOR}"
+        echo -e "${IINFO}Creating commit."
+    else
+        echo -e "${IINFO}Creating given commit."
+    fi
     
     echo -e "${IINFO}Saving changes."
     echo -e "${TAB}⋆ ${MAGENTA}${IBRCH}${pattern}${END_COLOR}"
